531b8059c99ffbc4c0f71df72531904c1ec0b5e5
Jaroslaw Konik <konikjar@gmail.com>
2024-05-11 13:59:44 +0200

    add test for calling rust function from rhai

--- tests/tests_rhai.rs
+++ tests/tests_rhai.rs
@@ -3,7 +3,8 @@ use bevy_scriptum::{prelude::*, Script, ScriptData, ScriptingRuntime};
 
 fn build_test_app() -> App {
     let mut app = App::new();
-    app.add_plugins(DefaultPlugins).add_plugins(ScriptingPlugin);
+    app.add_plugins((AssetPlugin::default(), TaskPoolPlugin::default()))
+        .add_plugins(ScriptingPlugin);
     app.update();
     app
 }
@@ -16,11 +17,9 @@ fn test_rhai_function_gets_called_from_rust() {
     let asset = asset_server.load("tests/rhai_function_gets_called_from_rust.rhai");
     let entity_id = app.world.spawn(Script::new(asset)).id();
 
-    app.update(); // Execute plugin internal systems
-
-    app.add_systems(Update, call_rhai_on_update_from_rust);
-
-    app.update(); // Execute test tystem
+    run_scripting_with(&mut app, |app| {
+        app.add_systems(Update, call_rhai_on_update_from_rust);
+    });
 
     let script_data = app.world.get::<ScriptData>(entity_id).unwrap();
     let state = script_data.scope.get_value::<rhai::Map>("state").unwrap();
@@ -35,4 +34,42 @@ fn call_rhai_on_update_from_rust(
     scripting_runtime
         .call_fn("test_func", &mut script_data, entity, ())
         .unwrap();
+}
+
+fn run_scripting_with(app: &mut App, f: impl FnOnce(&mut App)) {
+    app.update(); // Execute plugin internal systems
+    f(app);
+    app.update(); // Execute systems added by callback
+}
+
+#[test]
+fn test_rust_function_gets_called_from_rhai() {
+    let mut app = build_test_app();
+
+    #[derive(Default, Resource)]
+    struct TimesCalled {
+        times_called: u8,
+    }
+
+    app.world.init_resource::<TimesCalled>();
+
+    app.add_script_function(String::from("rust_func"), |mut res: ResMut<TimesCalled>| {
+        res.times_called += 1;
+    });
+
+    let asset_server = app.world.get_resource_mut::<AssetServer>().unwrap();
+    let asset = asset_server.load("tests/rust_function_gets_called_from_rhai.rhai");
+    app.world.spawn(Script::new(asset));
+
+    run_scripting_with(&mut app, |app| {
+        app.add_systems(Update, call_rhai_on_update_from_rust);
+    });
+
+    assert_eq!(
+        app.world
+            .get_resource::<TimesCalled>()
+            .unwrap()
+            .times_called,
+        1
+    );
 }
