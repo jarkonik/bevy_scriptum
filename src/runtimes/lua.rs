81e51dfdfcfc0c74511d40aa04b98be532213ed1
Jaroslaw Konik <konikjar@gmail.com>
2024-06-13 19:49:35 +0200

    cleanup

--- src/runtimes/lua.rs
+++ src/runtimes/lua.rs
@@ -11,8 +11,6 @@ use mlua::{
 use serde::Deserialize;
 use std::sync::{Arc, Mutex};
 
-// TODO: add example and implementation for builting types vec3 and others?
-
 use crate::{
     assets::GetExtensions,
     callback::{FromRuntimeValueWithEngine, IntoRuntimeValueWithEngine},
