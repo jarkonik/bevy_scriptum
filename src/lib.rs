5f0c22192b33f74aeb8d6d6b17dfbefa8eef027d
Dansion <dansion84@gmail.com>
2024-07-12 12:52:16 +0200

    Finalized documentation and examples

--- src/lib.rs
+++ src/lib.rs
@@ -82,7 +82,37 @@
 //! ```lua
 //! fun_with_string_param("Hello world!")
 //! ```
+//!It is also possible to split the definition of your callback functions up over multiple plugins. This enables you to split up your code by subject and keep the main initialization light and clean.
+//!This can be accomplished by using `add_scripting_api`. Be careful though, `add_scripting` has to be called before adding plugins.
+//!```rust 
+//!use bevy::prelude::*;
+//!use bevy_scriptum::prelude::*;
+//!use bevy_scriptum::runtimes::lua::prelude::*;
 //!
+//!struct MyPlugin;
+//!impl Plugin for MyPlugin {
+//!    fn build(&self, app: &mut App) {
+//!        app.add_scripting_api::<LuaRuntime>(|runtime| {
+//!            runtime.add_function(String::from("hello_from_my_plugin"), || {
+//!                info!("Hello from MyPlugin");
+//!            });
+//!        });
+//!    }
+//!}
+//!
+//!// Main
+//!fn main() {
+//!    App::new()
+//!        .add_plugins(DefaultPlugins)
+//!        .add_scripting::<LuaRuntime>(|_| {
+//!            // nice and clean
+//!        })
+//!        .add_plugins(MyPlugin)
+//!        .run();
+//!}
+//!```
+//! 
+//! 
 //! ## Usage
 //!
 //! Add the following to your `Cargo.toml`:
