b6e251dccad45873f5a74f3e84fa7e0a4c8b533e
Jaroslaw Konik <konikjar@gmail.com>
2025-05-20 07:00:00 +0200

    add todo

--- build.rs
+++ build.rs
@@ -1,3 +1,6 @@
 fn main() {
+    // export RUBY_CONFIGURE_OPTS="--disable-shared --disable-install-doc --disable-install-rdoc"
+    // ./configure --with-cc-opt=“-I/usr/local/ssl/include” –with-ld-opt=“-L/usr/local/ssl/lib -Wl,-Bstatic -lssl -lcrypto -Wl,-Bdynamic -ldl”
+
     println!("cargo:rustc-link-lib=z"); // TODO: if features Ruby
 }
